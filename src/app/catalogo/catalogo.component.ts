be83718c1c842c38f109fe777f1fb2bd586c4bdc
drivas23 <drivas@indupal.com>
2023-08-16 18:57:30 -0600

    Creacion de proyecto laboratorio

---
+++ src/app/catalogo/catalogo.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-catalogo',
+  templateUrl: './catalogo.component.html',
+  styleUrls: ['./catalogo.component.css']
+})
+export class CatalogoComponent {
+  nombre1 = "Febe Martinez";
+  nombre2 = "Kelvin Molina";
+  nombre3 = "Jairo ruiz";
+
+}
+
+
